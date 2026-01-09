319e869b5e7dc6c1e22bc7206331f91ff1994d92
Merfij <kristaps03@yahoo.co.uk>
2026-01-09 21:59:58 +0100

    Added folders for better sorting

--- Assets/Scripts/Guard_AI/Checkpoints.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Checkpoints : MonoBehaviour
-{
-    int currentIndex;
-    public List<Transform> checkpoints;
-    public List<Transform> visitedTargets;
-}
